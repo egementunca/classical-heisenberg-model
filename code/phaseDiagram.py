4bb8e0410fdae91b9ee056d8b5690c88c5d45b0b
egementunca <egementunca@gmail.com>
2022-03-03 12:26:41 +0300

    added function for spatial dimension calculation

--- code/phaseDiagram.py
+++ code/phaseDiagram.py
@@ -1,18 +1,18 @@
 import QuenchedRandomness as qr
 import numpy as np
 
+def check(p=0.5, n=3):
 
+	data = []
 
-def check(p):
-
-	j_disorder = 1/8
+	j_disorder = 1
 	j_order = 10
 	j_new = (j_disorder+j_order)/2
 
-	f = qr.rgTrajectory(j_new, p, 9, 3, 25, 21, 25)
+	f = qr.rgTrajectory(j_new, p, n, 3, 1000, 21, 25)
 
-	if f[-1][0][0] == 1:
-		j_dis = j_new
+	if abs(f[-1][0][0]) == 1:
+		j_disorder = j_new
 		print('{} is disordered'.format(j_new))
 		data.append([j_new, 'disordered'])
 	else:
@@ -20,10 +20,10 @@ def check(p):
 		print('{} is ordered'.format(j_new))
 		data.append([j_new, 'ordered'])
 	for i in range(39):
-		j_new = (j_order + j_dis)/2
-		f = qr.rgTrajectory(j_new, p, 9, 3, 25, 21, 25)
-		if f[-1][0][0] == 1:
-			j_dis = j_new
+		j_new = (j_order + j_disorder)/2
+		f = qr.rgTrajectory(j_new, p, n, 3, 1000, 21, 25)
+		if abs(f[-1][0][0]) == 1:
+			j_disorder = j_new
 			print('{} is disordered'.format(j_new))
 			data.append([j_new, 'disordered'])
 		else:
@@ -35,12 +35,14 @@ def check(p):
 
 def phase_diagram():
 
-	file = open('phasediagram.txt','w')
+	file = open('partialdim.txt','w')
 	crit_J = []
 
-	for p in np.linspace(0.05,.5,100):
-		crit_val = check(p)[-1][0]
+	for n in np.arange(4,28):
+		crit_val = check(p=0.5, n=n)[-1][0]
 		crit_J.append(crit_val)
-		file.write('p:{} critical j is {} \n'.format(p,crit_val))
+		file.write('n:{} critical j is {} \n'.format(n,crit_val))
 	file.close()
 	return crit_J
+
+phase_diagram()
