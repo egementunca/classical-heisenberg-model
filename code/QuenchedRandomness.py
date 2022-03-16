6e46861118e26fa7672e2c2d149b27e0ca7d368f
egementunca <egementunca@gmail.com>
2022-03-16 19:04:23 +0300

    Seed Problems Solved and Consistent Bond Move Function Added

--- code/QuenchedRandomness.py
+++ code/QuenchedRandomness.py
@@ -29,6 +29,23 @@ def poolBM(pool, n):
 	size, l_prec = len(pool), len(pool[0])
 	pools = []
 	pools.append(pool)
+
+	for i in range(n-1):
+		bm_step = []
+		for j in range(size):
+			lfc1, lfc2 = pools[0][random.randint(0,size-1)], pools[i][random.randint(0,size-1)]
+			lfc_bm = rg.bond_move(lfc1,lfc2,l_prec)
+			bm_step.append(lfc_bm)
+		pools.append(bm_step)
+	return pools[-1]
+
+#Bond Move for pool: Creates a pool n times  bond moved lfc's
+#(n = scaling factor^(dimension-1))
+def poolBM_old(pool, n):
+
+	size, l_prec = len(pool), len(pool[0])
+	pools = []
+	pools.append(pool)
 	n = bin(n)[:1:-1]
 	indices = [i for i, x in enumerate(n) if x == "1"]
 
@@ -65,7 +82,8 @@ def poolDEC(pool, dim):
 	for i in range(dim-1):
 		dec_step = []
 		for j in range(size):
-			lfc1, lfc2 = pools[0][random.randint(0,size-1)], pools[i][random.randint(0,size-1)]
+			ix1, ix2 = random.randint(0,size-1), random.randint(0,size-1)
+			lfc1, lfc2 = pools[0][ix1], pools[i][ix2]
 			lfc_dec = rg.decimate(lfc1,lfc2)
 			dec_step.append(lfc_dec)
 		pools.append(dec_step)
@@ -75,14 +93,15 @@ def poolDEC(pool, dim):
 #Renormalization Group with given Bond Moving and Decimaiton numbers
 def rgTransform(pool, dim, n):
 
+	random.seed(17)
 	pool_bond_moved = poolBM(pool, n)
+	random.seed(34)
 	pool_transformed = poolDEC(pool_bond_moved, dim)
 	return pool_transformed
 
 #Main function to track RG flows
 def rgTrajectory(J, p, n, dim, pool_size, l_prec, rg_step):
-
-	random.seed(17)
+	
 	LFC_flow = []
 
 	pool = startPool(J, p, pool_size, l_prec)
--- code/phaseDiagram.py
+++ code/phaseDiagram.py
@@ -4,22 +4,11 @@ import numpy as np
 def check(p=0.5, n=3):
 
 	data = []
-
+	
 	j_disorder = 1
 	j_order = 10
-	j_new = (j_disorder+j_order)/2
 
-	f = qr.rgTrajectory(j_new, p, n, 3, 1000, 21, 25)
-
-	if abs(f[-1][0][0]) == 1:
-		j_disorder = j_new
-		print('{} is disordered'.format(j_new))
-		data.append([j_new, 'disordered'])
-	else:
-		j_order = j_new
-		print('{} is ordered'.format(j_new))
-		data.append([j_new, 'ordered'])
-	for i in range(39):
+	for i in range(16):
 		j_new = (j_order + j_disorder)/2
 		f = qr.rgTrajectory(j_new, p, n, 3, 1000, 21, 25)
 		if abs(f[-1][0][0]) == 1:
@@ -33,9 +22,10 @@ def check(p=0.5, n=3):
 
 	return data
 
+
 def phase_diagram():
 
-	file = open('partialdim.txt','w')
+	file = open('partialdim.txt','a')
 	crit_J = []
 
 	for n in np.arange(4,28):
@@ -44,5 +34,3 @@ def phase_diagram():
 		file.write('n:{} critical j is {} \n'.format(n,crit_val))
 	file.close()
 	return crit_J
-
-phase_diagram()
